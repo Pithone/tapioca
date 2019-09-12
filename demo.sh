a9eac320608f9c4cc4f5c3f7ea1c9df54c02107d
Niccolò Izzo <n1zzo@users.noreply.github.com>
2019-09-12 21:50:43 +0200

    Update demo files

--- demo.sh
+++ demo.sh
@@ -1,10 +1,10 @@
-cd ~/tapioca/rpi-rgb-led-matrix/examples-api-use
-
 # Reset panels
 python3 resetmatrix.py
 
+cd ~/tapioca/rpi-rgb-led-matrix/examples-api-use
+
 # Image demo
-sudo ./demo --led-rows=32 --led-cols=64 --led-chain=2 --led-pixel-mapper="U-mapper;Rotate:90" --led-pwm-bits=11 --led-show-refresh --led-slowdown-gpio=0 --led-pwm-lsb-nanoseconds=100 --led-brightness=50 -D 4
+sudo ./demo --led-rows=32 --led-cols=64 --led-chain=2 --led-pixel-mapper="U-mapper;Rotate:90" --led-pwm-bits=11 --led-show-refresh --led-slowdown-gpio=0 --led-pwm-lsb-nanoseconds=100 --led-brightness=50 -D 7
 
 # Text demo
 sudo ./scrolling-text-example --led-cols=64 --led-rows=32 --led-chain=2 --led-pixel-mapper="U-pper;Rotate:90" --led-pwm-lsb-nanoseconds 130 --led-scan-mode=1 "Viva la figa" -f ../fonts/10x20.bdf -C 255,0,0
