9848a44c208dbc1467a42c76ca8e2d7bec3d5972
Niccolò Izzo <n1zzo@users.noreply.github.com>
2020-01-03 15:20:15 +0100

    Fix build process

--- src/tapioca.cc
+++ src/tapioca.cc
@@ -3,16 +3,27 @@
 #include <cpr/cpr.h>
 #include <nlohmann/json.hpp>
 #include <graphics.h>
+#include <toml.hpp>
+#include <led-matrix.h>
 
 enum leg_mode { TRAIN, WALK, TRAM, CAR, BUS, BIKE };
 
-/* TODO: Load configuration from toml */
 class Config {
     public:
         std::string start_point;
         std::string end_point;
         std::string region;
         bool got_bike;
+        bool terminal;
+
+        Config() {
+            const auto config = toml::parse("../config.toml");
+            start_point = toml::find<std::string>(config, "start");
+            end_point = toml::find<std::string>(config, "end");
+            region = toml::find<std::string>(config, "region");
+            got_bike = toml::find<bool>(config, "bike");
+            got_bike = toml::find<bool>(config, "terminal");
+        }
 };
 
 class Color {
@@ -20,6 +31,12 @@ class Color {
         unsigned char r;
         unsigned char g;
         unsigned char b;
+
+        Color(int r, int g, int b) {
+            this->r = r;
+            this->g = g;
+            this->b = b;
+        }
 };
 
 class Leg {
@@ -39,8 +56,8 @@ std::vector<Journey> get_journeys(Config config) {
     /* TODO: Use only SUGGESTED journeys, that can be found at
      * json["sections"][0]["journeys"] */
     auto response = cpr::Get(cpr::Url{"https://it-milan-api.citymapper.com/7/journeys"},
-                             cpr::Parameters{{"start", "45.496014,9.375752"},
-                                             {"end", "45.479091,9.233966"}});
+                             cpr::Parameters{{"start", config.start_point},
+                                             {"end", config.end_point}});
 
     auto json = nlohmann::json::parse(response.text);
     int j_idx = 0;
@@ -58,36 +75,45 @@ std::vector<Journey> get_journeys(Config config) {
     }
 }
 
-void print_header() {
+void print_header(Config config) {
     ;
 }
 
-void print_journey(Journey journey) {
+void print_journey(Config config, Journey journey) {
     ;
 }
 
-void print_footer() {
-    ;
-}
-
-Config load_config() {
+void print_footer(Config config) {
     ;
 }
 
 int main(int argc, char** argv) {
     /* Load configuration file */
-    Config config = load_config();
+    Config config = Config();
+
+    rgb_matrix::RGBMatrix::Options matrix_options;
+    rgb_matrix::RuntimeOptions runtime_opt;
+    rgb_matrix::ParseOptionsFromFlags(&argc,
+                                      &argv,
+                                      &matrix_options,
+                                      &runtime_opt);
+
+    rgb_matrix::RGBMatrix *matrix = rgb_matrix::CreateMatrixFromOptions(matrix_options,
+                                                            runtime_opt);
+
+    // TODO: implement brightness control
+    //matrix->SetBrightness(brightness);
 
     /* Continue until SIGSTOP is received */
     for(;;) {
         std::vector<Journey> journeys = get_journeys(config);
 
         /* Print data to matrix */
-        print_header();
+        print_header(config);
         for(auto journey : journeys) {
-            print_journey(journey);
+            print_journey(config, journey);
         }
-        print_footer();
+        print_footer(config);
 
         /* Wait for one minute before refreshing */
         std::this_thread::sleep_for(std::chrono::minutes(1));
